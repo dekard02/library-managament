bbfee001cacf7a8f7d76673fe497fd03ed7a225e
Dekard <dangkhoatanhao@gmail.com>
2023-05-15 23:10:20 +0700

    config

---
+++ src/main/java/com/dekard02/librarymanagement/ApplicationConfig.java
@@ -0,0 +1,20 @@
+package com.dekard02.librarymanagement;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import com.github.slugify.Slugify;
+
+@Configuration
+@EnableSpringDataWebSupport
+@EnableJpaAuditing
+public class ApplicationConfig {
+
+    @Bean
+    public Slugify slugify() {
+        return Slugify.builder().build();
+    }
+
+}
